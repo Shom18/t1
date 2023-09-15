d67b780f24562cab9cb1c0a8e159be8cdb35f05d
«Shom18» <kbshamil2002g@mail.ru>
2023-09-15 13:36:23 +0000

    add

---
+++ pg/src/prom.d
@@ -0,0 +1,25 @@
+src/prom.o: src/prom.c /usr/include/postgresql/14/server/postgres.h \
+ /usr/include/postgresql/14/server/c.h \
+ /usr/include/postgresql/14/server/postgres_ext.h \
+ /usr/include/postgresql/14/server/pg_config_ext.h \
+ /usr/include/postgresql/14/server/pg_config.h \
+ /usr/include/postgresql/14/server/pg_config_manual.h \
+ /usr/include/postgresql/14/server/pg_config_os.h \
+ /usr/include/postgresql/14/server/port.h \
+ /usr/include/postgresql/14/server/utils/elog.h \
+ /usr/include/postgresql/14/server/utils/errcodes.h \
+ /usr/include/postgresql/14/server/utils/palloc.h \
+ /usr/include/postgresql/14/server/utils/builtins.h \
+ /usr/include/postgresql/14/server/fmgr.h \
+ /usr/include/postgresql/14/server/nodes/nodes.h \
+ /usr/include/postgresql/14/server/utils/fmgrprotos.h \
+ /usr/include/postgresql/14/server/utils/timestamp.h \
+ /usr/include/postgresql/14/server/datatype/timestamp.h \
+ /usr/include/postgresql/14/server/pgtime.h \
+ /usr/include/postgresql/14/server/utils/jsonb.h \
+ /usr/include/postgresql/14/server/lib/stringinfo.h \
+ /usr/include/postgresql/14/server/utils/array.h \
+ /usr/include/postgresql/14/server/utils/expandeddatum.h \
+ /usr/include/postgresql/14/server/utils/numeric.h \
+ /usr/include/postgresql/14/server/utils/json.h src/prom.h src/parse.h \
+ src/utils.h
---
+++ pg/src/utils.d
@@ -0,0 +1,12 @@
+src/utils.o: src/utils.c /usr/include/postgresql/14/server/postgres.h \
+ /usr/include/postgresql/14/server/c.h \
+ /usr/include/postgresql/14/server/postgres_ext.h \
+ /usr/include/postgresql/14/server/pg_config_ext.h \
+ /usr/include/postgresql/14/server/pg_config.h \
+ /usr/include/postgresql/14/server/pg_config_manual.h \
+ /usr/include/postgresql/14/server/pg_config_os.h \
+ /usr/include/postgresql/14/server/port.h \
+ /usr/include/postgresql/14/server/utils/elog.h \
+ /usr/include/postgresql/14/server/utils/errcodes.h \
+ /usr/include/postgresql/14/server/utils/palloc.h \
+ /usr/include/postgresql/14/server/datatype/timestamp.h src/utils.h
